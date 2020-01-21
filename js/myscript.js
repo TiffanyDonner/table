a3e56dcbad3dc3fecda1783c7dfb9c9a8346893d
Tiffany Donner <tiffany.donner@stsupport.se>
2020-01-21 16:38:12 +0000

    challenge completed for table

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-    $("h2").addClass("underline"); //underlines all <h2> elements
-    $("ul").addClass("border"); //adds border to all lists
+    $("tr:odd").addClass("odd");
+    $("tr:even").addClass("even");
 });
