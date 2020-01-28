f82567801008b96c309a3f4136979f251f215421
Tiffany Donner <tiffany.donner@stsupport.se>
2020-01-28 10:27:23 +0000

    Changed index and myscript file to reflect Other Traversing Methods - Challenge 2

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
     $("tr:odd").addClass("odd");
     $("tr:even").addClass("even");
+
+    /*when table header is clicked , removes selection class from all other table rows and
+      adds the selection class to this table headers row only*/
+     $("th").click(function(){
+     	$("tr").children().removeClass("selection")
+    	$(this).siblings().addClass("selection");
+
+  });
 });
